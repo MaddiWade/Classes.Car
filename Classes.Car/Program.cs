a5d19c39f6b9e78840973d72e85283fab76ee596
Maddi Wade <Madelinenwade@gmail.com>
2022-02-21 10:47:25 -0500

    finished!

--- Classes.Car/Program.cs
+++ Classes.Car/Program.cs
@@ -6,7 +6,13 @@ namespace Classes.Car
     {
         static void Main(string[] args)
         {
-            
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Explorer";
+            myCar.Year = 2022;
+            Console.WriteLine($"my car is a {myCar.Make} {myCar.Model} , its a {myCar.Year}");
         }
+
+        
     }
 }
